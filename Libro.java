c1ee0a4339a36904cb6bffebfde5588f79719ab8
ebergdx <garcia.sanchez.eber453@gmail.com>
2025-09-25 19:56:19 -0600

    Update Libro.java
    Cambié el atributo estático y otras cositas

--- Libro.java
+++ Libro.java
@@ -1,107 +1,125 @@
 public class Libro{
 
-	//Atributos de la clase
-	//Datos del libro
-   	private String titulo;
-    	private String autor;
-    	private String genero;
-    	private int id;
-    	private double precio;
-    	private boolean estado;  //true = disponible, false = no disponible
-	//Control para IDs automáticos
-	private static int contadorIds = 1;
+    //Atributos de la clase
+    //Datos del libro
+    private String titulo;
+    private String autor;
+    private String genero;
+    private int id;
+    private double precio;
+    private boolean estado; //true = disponible, false = no disponible
+    private String devolucion;
     
-	//Constructor completo para registrar un nuevo libro
-    	public Libro(String titulo, String autor, String genero, int id, double precio, boolean estado){
-        	this.titulo = titulo;
-        	this.autor = autor;
-        	this.genero = genero;
-        	this.id = contadorIds++;;
-        	this.precio = precio;
-        	this.estado = estado;  //Usa el valor del parámetro
-    	}
+    //Control para IDs automáticos
+    private static int contadorIds = 1;
+    
+    //Constructor completo para registrar un nuevo libro
+    public Libro(String titulo, String autor, String genero, int id, double precio, boolean estado){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.genero = genero;
+        this.precio = precio;
+        this.estado = estado;  //Usa el valor del parámetro
+        this.devolucion = "";
+    }
 
-	//Segundo Constructor (para usar valor por defecto al precio y estado disponible)
+    //Segundo Constructor (para usar valor por defecto al precio y estado disponible)
 
-    	public Libro(String titulo, String autor, String genero){
-        	this.titulo = titulo;
-        	this.autor = autor;
-        	this.genero = genero;
-        	this.id = contadorIds++;
-        	this.precio = 149.99;  //Por defecto $149.99
-        	this.estado = true;  //Por defecto disponible
-    	}
+    public Libro(String titulo, String autor, String genero){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.genero = genero;
+        this.precio = 149.99;  //Por defecto $149.99
+        this.estado = true;  //Por defecto disponible
+        this.devolucion = "";
+    }
     
-    	//Getters (para ver la información)
-    
-    	public String getTitulo(){
-        	return titulo;
-    	}
-    
-    	public String getAutor(){
-        	return autor;
-    	}
-    
-    	public String getGenero(){
-        	return genero;
-    	}
-    
-    	public int getId(){
-        	return id;
-    	}
-    
-    	public double getPrecio(){
-        	return precio;
-    	}
-    
-   	 public boolean getEstado(){
-        	return estado;
-    	}
-    
-	//Setters (para actualizar información)
-	public void setTitulo(String titulo){
-		this.titulo = titulo;
-	}
-	
-	public void setAutor(String autor){
-		this.autor = autor;
-	}
+    //Método para aumentar el contador
+    public static void incrementarId(){
+        contadorIds++;
+    }
 
-	public void setGenero(String genero){
-		this.genero = genero;
-	}
-	
-	public void setPrecio(double precio){
-		this.precio = precio;
-	}
+        //Getters (para ver la información)
+    
+        public String getTitulo(){
+            return titulo;
+        }
+    
+        public String getAutor(){
+            return autor;
+        }
+    
+        public String getGenero(){
+            return genero;
+        }
+    
+        public int getId(){
+            return id;
+        }
+    
+        public double getPrecio(){
+            return precio;
+        }
+    
+        public boolean getEstado(){
+            return estado;
+        }
 
-	public void setEstado(boolean estado){
-		this.estado = estado;
-	}
+        public String devolucion(){
+            return devolucion;
+        }
+    
+    //Setters (para actualizar información)
+    public void setTitulo(String titulo){
+        this.titulo = titulo;
+    }
+    
+    public void setAutor(String autor){
+        this.autor = autor;
+    }
 
-	//Métodos especiales del negocio
-	//Para marcar libro como disponible
-	public void marcarDisponible(){
-		this.estado = true;
-	}
+    public void setGenero(String genero){
+        this.genero = genero;
+    }
+    
+    public void setPrecio(double precio){
+        this.precio = precio;
+    }
 
-	//Para marcar libro como no disponible
-	public void marcarNoDisponible(){
-		this.estado = false;
-	}
+    public void setEstado(boolean estado){
+        this.estado = estado;
+    }
 
-	//Para verificar si el libro está disponible
-	public boolean estaDisponible(){
-		return estado;
-	}
-		
-	//Para mostrar la información del libro
-   	public String toString(){
-		String disponibilidad = estado ? "Disponible" : "No disponible";
-        	return "Libro #" + id + ": " + titulo +
-        		" | Autor: " + autor + 
-        		" | Género: " + genero + 
-        		" | Precio: $" + precio +
-        		" | Estado: "+ disponibilidad;
-    	}
+    //Métodos especiales del negocio
+    //Para marcar libro como disponible
+    public void marcarDisponible(){
+        this.estado = true;
+    }
+
+    //Para marcar libro como no disponible
+    public void marcarNoDisponible(){
+        this.estado = false;
+    }
+
+    //Para verificar si el libro está disponible
+    public boolean estaDisponible(){
+        return estado;
+    }
+
+    public String devolucion(){
+        return devolucion;
+    }
+        
+    //Para mostrar la información del libro
+    public String toString(){
+        String disponibilidad = estado ? "Disponible" : "No disponible";
+            return "Libro #" + id + ": " + titulo +
+                " | Autor: " + autor + 
+                " | Género: " + genero + 
+                " | Precio: $" + precio +
+                " | Estado: "+ disponibilidad +
+                if(estado == false && devolucion.isEmpty() == true){
+                    System.out.println("| Fecha de devolucion: " + devolucion);
+                };
+    }
 }
